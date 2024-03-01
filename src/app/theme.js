0f31254454610be09bbf061ad204d2231ab92377
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-01 08:33:59 +0100

    created initial theme.js file with colors of the project and default mobileMax breakpoint

---
+++ src/app/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    lightGray: "#F5F5FA",
+    gray: "#E4E6F0",
+    silver: "#C4C4C4",
+    darkGray: "#7E839A",
+    slateGray: "#74788B",
+    almostBlack: "#18181B",
+    black: "#000000",
+    lightBlue: "#D6E4FF",
+    blue: "#0044CC",
+  },
+  breakpoint: {
+    mobileMax: 767,
+  },
+};
