fcb62bc010fe9e42608997345e07af8892ee2411
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-05 17:01:10 +0100

    submit handler and input is now a controlled component

--- src/features/SiteHeader/SearchBar/index.js
+++ src/features/SiteHeader/SearchBar/index.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import { InputWrapper, StyledForm, StyledInput, StyledSearchIcon } from "./styled";
 
-const SearchBar = () => (
-  <StyledForm>
-    <InputWrapper>
-      <StyledSearchIcon />
-      <StyledInput placeholder={"Search for movies..."} />
-    </InputWrapper>
-  </StyledForm>
-);
+const SearchBar = () => {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+};
+
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <InputWrapper>
+        <StyledSearchIcon />
+        <StyledInput 
+          placeholder={"Search for movies..."}
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          />
+      </InputWrapper>
+    </StyledForm>
+  );
+}
 
 export default SearchBar;
