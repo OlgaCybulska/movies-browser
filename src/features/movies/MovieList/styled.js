d10153ff19164121c211e6647c55223628d6f6fa
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-07 16:36:53 +0100

    added GridWrapper component styling for new breakpoint "laptopMax"

--- src/features/movies/MovieList/styled.js
+++ src/features/movies/MovieList/styled.js
@@ -5,6 +5,12 @@ export const GridWrapper = styled.div`
   grid-gap: 24px;
   grid-template-columns: repeat(4, 1fr);
 
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptopMax}px) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 20px;
+    padding: 0 16px;
+  }
+
   @media (max-width: ${({ theme }) => theme.breakpoint.tabletHorizontalMax}px) {
     grid-template-columns: repeat(3, 1fr);
     grid-gap: 20px;
