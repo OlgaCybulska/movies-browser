1415b38091d13214d9e4731c2f0a5effd12ae771
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-07 14:41:43 +0100

    added media queries to GridWrapper component in MovieList

--- src/features/movies/MovieList/styled.js
+++ src/features/movies/MovieList/styled.js
@@ -4,4 +4,20 @@ export const GridWrapper = styled.div`
   display: grid;
   grid-gap: 24px;
   grid-template-columns: repeat(4, 1fr);
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tabletHorizontalMax}px) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 20px;
+    padding: 0 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tabletVerticalMax}px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 18px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    grid-template-columns: 1fr;
+    grid-gap: 16px;
+  }
 `;
