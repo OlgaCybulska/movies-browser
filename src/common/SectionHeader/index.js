2f4b6dfdee936f0d87af57f88443f6ccda046497
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-07 14:39:09 +0100

    added media queries to SectionHeader component

--- src/common/SectionHeader/index.js
+++ src/common/SectionHeader/index.js
@@ -4,4 +4,13 @@ export const SectionHeader = styled.h1`
   font-weight: 600;
   font-size: 36px;
   color: ${({ theme }) => theme.color.blackPearl};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tabletHorizontalMax}px) {
+    padding-left: 16px;
+    font-size: 26px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 20px;
+  }
 `;
