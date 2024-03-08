61c135a10a4649d5cd8d7eb75bf00ca7537a52b1
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-08 14:08:43 +0100

    disabled MovieList rendering and left LoadingPage visible for good presentation while coding

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { GlobalStyle } from "./app/GlobalStyle";
 import { theme } from "./app/theme";
 import { MovieList } from "./features/movies/MovieList";
 import SiteHeader from "./features/SiteHeader";
+import { LoadingPage } from "./common/LoadingPage";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <SiteHeader />
-      <MovieList />
+      {/* <MovieList /> */}
+      <LoadingPage />
     </ThemeProvider>
   );
 }
