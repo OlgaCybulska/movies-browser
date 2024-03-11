2cb5db698afb85b8f8c374c44f2ebeeb76f6cdb5
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-11 22:03:31 +0100

    added back SiteHeader

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Provider store={store}>
+        <SiteHeader />
         <GlobalStyle />
         <MovieList />
       </Provider>
