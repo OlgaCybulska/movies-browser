691ef76f5ac710049f0f4169a15c76634a46cacf
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-12 11:09:34 +0100

    placeholder PeopleList component

---
+++ src/features/people/PeopleList/index.js
@@ -0,0 +1,16 @@
+import { Container } from "../../../common/Container";
+import { Section } from "../../../common/Section";
+import { SectionHeader } from "../../../common/SectionHeader";
+import { Tile } from "../../../common/Tile";
+
+const PeopleList = () => {
+  <Container>
+    <Section>
+      <SectionHeader>
+        Popular people
+      </SectionHeader>
+    </Section>
+  </Container>
+};
+
+export default PeopleList;
