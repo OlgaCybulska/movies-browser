386d22c12706eecdbdae8c6df35f49d336b2e54b
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-13 13:16:55 +0100

    added mapping for MovieList rendering based on API data, added conditional switch rendering function that result depends of API data status

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -6,34 +6,46 @@ import { SectionHeader } from "../../../common/SectionHeader";
 import { Tile } from "../../../common/Tile";
 import { fetchMovies } from "./moviesListSlice";
 import { GridWrapper } from "./styled";
+import { selectMovies, selectStatus } from "./moviesListSlice";
+import { LoadingPage } from "../../../common/LoadingPage";
+import { ErrorPage } from "../../../common/ErrorPage";
 
 export const MovieList = () => {
   const dispatch = useDispatch();
+  const popularMovies = useSelector(selectMovies);
+  const status = useSelector(selectStatus);
 
   useEffect(() => {
     dispatch(fetchMovies());
   }, [dispatch]);
 
-  return (
-    <Container>
-      <Section>
-        <SectionHeader>Popular movies</SectionHeader>
-        <GridWrapper>
-          <Tile
-            title="Mulan very long title"
-            year="2020"
-            rate="7,8"
-            votes="35"
-          />
-          <Tile title="Mulan" year="2020" rate="7,8" votes="35" />
-          <Tile title="Mulan" year="2020" rate="7,8" votes="35" />
-          <Tile title="Mulan" year="2020" rate="7,8" votes="35" />
-          <Tile title="Mulan" year="2020" rate="7,8" votes="35" />
-          <Tile title="Mulan" year="2020" rate="7,8" votes="35" />
-          <Tile title="Mulan" year="2020" rate="7,8" votes="35" />
-          <Tile title="Mulan" year="2020" rate="7,8" votes="35" />
-        </GridWrapper>
-      </Section>
-    </Container>
-  );
+  switch (status) {
+    case "initial":
+      return null;
+    case "loading":
+      return <LoadingPage />;
+    case "error":
+      return <ErrorPage />;
+    case "success":
+      return (
+        <Container>
+          <Section>
+            <SectionHeader>Popular movies</SectionHeader>
+            <GridWrapper>
+              {popularMovies.map((movie) => (
+                <li key={movie.id}>
+                  <Tile
+                    posterPath={movie.poster_path}
+                    title={movie.original_title}
+                    year={movie.release_date.slice(0, 4)}
+                    rate={movie.vote_average.toFixed(1)}
+                    votes={movie.vote_count}
+                  />
+                </li>
+              ))}
+            </GridWrapper>
+          </Section>
+        </Container>
+      );
+  }
 };
