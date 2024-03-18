a6557cbfc7b9efcc861f0f7f917b1750a6521df7
Olga Cybulska <olgacybulska7@wp.pl>
2024-03-18 22:58:56 +0100

    page to URL added

--- src/features/movies/MovieList/getMovies.js
+++ src/features/movies/MovieList/getMovies.js
@@ -1,7 +1,7 @@
 import { getMoviesURL } from "../../../utils/API/getMoviesURL";
 
-export const getMovies = async () => {
-  const response = await fetch(getMoviesURL);
+export const getMovies = async (page) => {
+  const response = await fetch(`${getMoviesURL}}&page=${page}`);
 
   if (!response.ok) {
     new Error(response.statusText);
