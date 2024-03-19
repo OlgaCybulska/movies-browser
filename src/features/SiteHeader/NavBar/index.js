74eb58fb9bcdc85017b95fcb98977baf930bbd11
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-19 16:59:52 +0100

    search mode is read from current url path using useLocation hook

--- src/features/SiteHeader/NavBar/index.js
+++ src/features/SiteHeader/NavBar/index.js
@@ -1,20 +1,32 @@
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
 import { StyledList, StyledListItem, StyledNav, StyledNavLink } from "./styled";
+import { useDispatch } from "react-redux";
+import { setSearchMode } from "./navBarSlice";
 
-const NavBar = () => (
-  <StyledNav>
-    <StyledList>
-      <StyledNavLink to="/movies">
-        <StyledListItem>
-          MOVIES
-        </StyledListItem>
-      </StyledNavLink>
-      <StyledNavLink to="/people">
-        <StyledListItem>
-          PEOPLE
-        </StyledListItem>
-      </StyledNavLink>
-    </StyledList>
-  </StyledNav>
-);
+const NavBar = () => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  const searchKeyword = location.pathname.split("/")[1];
+
+  dispatch(setSearchMode(searchKeyword));
+
+  return (
+    <StyledNav>
+      <StyledList>
+        <StyledNavLink to="/movies">
+          <StyledListItem>
+            MOVIES
+          </StyledListItem>
+        </StyledNavLink>
+        <StyledNavLink to="/people">
+          <StyledListItem>
+            PEOPLE
+          </StyledListItem>
+        </StyledNavLink>
+      </StyledList>
+    </StyledNav>
+  );
+}
 
 export default NavBar;
