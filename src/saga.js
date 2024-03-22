7deacb74b4d6bd9826d8e667030ba8f728b4023d
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-22 14:10:07 +0100

    removed movie slice and saga

--- src/saga.js
+++ src/saga.js
@@ -1,11 +1,9 @@
 import { all } from "redux-saga/effects";
-import { moviesListSaga } from "./features/movies/MovieList/moviesListSaga";
 import { paginationSaga } from "./common/Pagination/paginationSaga";
 import { dataSaga } from "./utils/API/dataSaga";
 
 export default function* rootSaga() {
   yield all([
-    moviesListSaga(),
     paginationSaga(),
     dataSaga()
   ]);
--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./saga";
-import moviesListReducer from "./features/movies/MovieList/moviesListSlice";
 import navBarReducer from "./features/SiteHeader/NavBar/navBarSlice";
 import paginationReducer from "./common/Pagination/paginationSlice";
 import dataReducer from "./utils/API/dataSlice";
@@ -10,7 +9,6 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
-    moviesList: moviesListReducer,
     data: dataReducer,
     navBar: navBarReducer,
     pagination: paginationReducer,
