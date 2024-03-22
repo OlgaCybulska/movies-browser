8a603ad8459accfbbb8eb277accefbe522b276da
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-22 13:54:08 +0100

    added universal dataSaga for fetch data

---
+++ src/utils/API/dataSaga.js
@@ -0,0 +1,25 @@
+import { call, delay, put, takeLatest } from "redux-saga/effects";
+import { getData } from "../../../utils/API/getData";
+import {
+  fetchData,
+  fetchDataSuccess,
+  fetchDataError,
+} from "./dataSlice";
+
+function* fetchDataHandler(action) {
+  try {
+    const apiData = yield call(getData, action.payload);
+    yield delay(1000);
+    if (apiData.success === false) {
+      throw new Error();
+    }
+    yield put(fetchDataSuccess(movies));
+  } catch (error) {
+    console.log(error);
+    yield put(fetchDataError());
+  }
+}
+
+export function* dataSaga() {
+  yield takeLatest(fetchData.type, fetchDataHandler);
+}
