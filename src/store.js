b1935f4c7ce783a9bb48339e490dc37719be28dc
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-22 13:47:44 +0100

    added universal dataSlice

--- src/store.js
+++ src/store.js
@@ -4,12 +4,14 @@ import rootSaga from "./saga";
 import moviesListReducer from "./features/movies/MovieList/moviesListSlice";
 import navBarReducer from "./features/SiteHeader/NavBar/navBarSlice";
 import paginationReducer from "./common/Pagination/paginationSlice";
+import dataReducer from "./utils/API/dataSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     moviesList: moviesListReducer,
+    data: dataReducer,
     navBar: navBarReducer,
     pagination: paginationReducer,
   },
