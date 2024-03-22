3585b94f171c09d94a243394ee6cba94da6df1bd
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-22 15:19:31 +0100

    placeholder peoplelist (working api)

--- src/features/people/PeopleList/index.js
+++ src/features/people/PeopleList/index.js
@@ -1,15 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+import { useEffect } from "react";
 import { Container } from "../../../common/Container";
 import { Section } from "../../../common/Section";
 import { SectionHeader } from "../../../common/SectionHeader";
 import { useDataURL } from "../../../utils/API/useDataURL";
 import { Pagination } from "../../../common/Pagination";
-import { useEffect } from "react";
-import { fetchData, selectDataList } from "../../../utils/API/dataSlice";
+import { fetchData, selectDataList, selectStatus } from "../../../utils/API/dataSlice";
+import { LoadingPage } from "../../../common/LoadingPage";
+import { ErrorPage } from "../../../common/ErrorPage";
 
 const PeopleList = () => {
   const dispatch = useDispatch();
   const dataURL = useDataURL();
+  const status = useSelector(selectStatus);
 
   useEffect(() => {
     dispatch(fetchData(dataURL));
@@ -17,31 +21,34 @@ const PeopleList = () => {
 
   const popularActors = useSelector(selectDataList);
 
-  return (
-    <>
-      <Container>
-        <Section>
-          <SectionHeader>
-            Popular people
-          </SectionHeader>
-          {/* {popularActors.map((actor) => (
-                  <li key={actor.id}>
-                    <StyledLink to={`/movies/${actor.id}`}>
-                      <Tile
-                        posterPath={actor.poster_path}
-                        title={actor.original_title}
-                        year={actor.release_date.slice(0, 4)}
-                        rate={actor.vote_average.toFixed(1)}
-                        votes={actor.vote_count}
-                      />
-                    </StyledLink>
-                  </li>
-                ))} */}
-        </Section>
-      </Container>
-      <Pagination />
-    </>
-  );
+  switch (status) {
+    case "initial":
+      return null;
+    case "loading":
+      return <LoadingPage />;
+    case "error":
+      return <ErrorPage />;
+    case "success":
+      return (
+        <>
+          <Container>
+            <Section>
+              <SectionHeader>
+                Popular people
+              </SectionHeader>
+              {popularActors.map((actor) => (
+                <li key={actor.id}>
+                  <Link to={`/people/${actor.id}`}>
+                    {actor.name}
+                  </Link>
+                </li>
+              ))}
+            </Section>
+          </Container>
+          <Pagination />
+        </>
+      );
+  }
 }
 
 export default PeopleList;
