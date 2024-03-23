d081ce1ffbaf71fa59365d55937047c37555c11a
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-23 14:39:35 +0100

    fetching movie details

--- src/features/movies/MovieDetails/index.js
+++ src/features/movies/MovieDetails/index.js
@@ -1,10 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Section } from "../../../common/Section";
 import { SectionHeader } from "../../../common/SectionHeader";
+import { useDataURL } from "../../../utils/API/useDataURL";
+import { useEffect } from "react";
+import { fetchData, selectDataDetails } from "../../../utils/API/dataSlice";
 
-const MovieDetails = () => (
-  <Section>
-    <SectionHeader>Movie details</SectionHeader>
-  </Section>
-);
+const MovieDetails = () => {
+  const dispatch = useDispatch();
+  const dataURL = useDataURL();
+
+  useEffect(() => {
+    dispatch(fetchData(dataURL));
+  }, [dispatch, dataURL]);
+
+  const movieDetails = useSelector(selectDataDetails);
+
+  return (
+    <Section>
+      <SectionHeader>{movieDetails.original_title}</SectionHeader>
+    </Section>
+  );
+}
 
 export default MovieDetails;
