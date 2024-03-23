c4e1fd78cb5006ab92eb1ad91ea61731f1717f52
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-23 15:00:50 +0100

    added people details fetching

--- src/features/people/PeopleDetails/index.js
+++ src/features/people/PeopleDetails/index.js
@@ -1,10 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Section } from "../../../common/Section";
 import { SectionHeader } from "../../../common/SectionHeader";
+import { useDataURL } from "../../../utils/API/useDataURL";
+import { fetchData, selectDataDetails } from "../../../utils/API/dataSlice";
+import { useEffect } from "react";
 
-const PeopleDetails = () => (
-  <Section>
-    <SectionHeader>Movie details</SectionHeader>
-  </Section>
-);
+const PeopleDetails = () => {
+  const dispatch = useDispatch();
+  const dataURL = useDataURL();
+
+  useEffect(() => {
+    dispatch(fetchData(dataURL));
+  }, [dispatch, dataURL]);
+
+  const peopleDetails = useSelector(selectDataDetails);
+
+  return (
+    <Section>
+      <SectionHeader>{peopleDetails.name}</SectionHeader>
+    </Section>
+  );
+};
 
 export default PeopleDetails;
