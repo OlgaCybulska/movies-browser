96c45b1a139d9463baf8eba8e858385e712cbc5b
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-28 16:18:09 +0100

    added one format function and rendering backdrop and DetailTile based on API data

--- src/features/movies/MovieDetails/index.js
+++ src/features/movies/MovieDetails/index.js
@@ -3,7 +3,12 @@ import { Section } from "../../../common/Section";
 import { SectionHeader } from "../../../common/SectionHeader";
 import { useDataURL } from "../../../utils/API/useDataURL";
 import { useEffect } from "react";
-import { fetchAdditionalData, fetchData, selectData, selectStatus } from "../../../utils/API/dataSlice";
+import {
+  fetchAdditionalData,
+  fetchData,
+  selectData,
+} from "../../../utils/API/dataSlice";
+import { backdropURL, posterURL } from "../../../utils/API/apiDataURLs";
 import { useAdditionalDataURL } from "../../../utils/API/useAdditionalDataURL";
 import { SmallGridWrapper } from "../../../common/Tile/styled";
 import { Container } from "../../../common/Container";
@@ -23,23 +28,43 @@ const MovieDetails = () => {
   }, [dispatch, dataURL, additionalDataURL]);
 
   const movieDetails = useSelector(selectData);
+  console.log(movieDetails);
+
+  // Formatting functions
+  const formatDate = (dateString) => {
+    const [year, month, day] = dateString.split("-");
+    return `${day}.${month}.${year}`;
+  };
 
   return (
     <>
-      <BackdropSection title="Mulan" votes="335" rate="7,8" />
-      <Container>
-        <DetailTile
-          posterPath={examplePoster}
-          title="Mulan"
-          subtitle="2020"
-          firstData="China, UnitedStates of America"
-          secondData="24.10.2020"
-          votes="335"
-          rate="7,8"
-          isOnBackdrop={false}
-          isOnMainTile={true}
-          overview="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+      {movieDetails.backdrop_path && (
+        <BackdropSection
+          backgroundURL={`${backdropURL}${movieDetails.backdrop_path}`}
+          title={movieDetails.title}
+          votes={movieDetails.vote_count}
+          rate={movieDetails.vote_average.toFixed(1)}
         />
+      )}
+
+      <Container>
+        {movieDetails.poster_path && (
+          <DetailTile
+            posterPath={`${posterURL}${movieDetails.poster_path}`}
+            title={movieDetails.title}
+            subtitle={movieDetails.release_date.split("-")[0]}
+            firstData={movieDetails.production_countries
+              .map((country) => country.name)
+              .join(", ")}
+            secondData={formatDate(movieDetails.release_date)}
+            votes={movieDetails.vote_count}
+            rate={movieDetails.vote_average.toFixed(1)}
+            isOnBackdrop={false}
+            isOnMainTile={true}
+            overview={movieDetails.overview}
+          />
+        )}
+
         <Section>
           <SectionHeader>Cast</SectionHeader>
           <SmallGridWrapper>
@@ -121,6 +146,6 @@ const MovieDetails = () => {
       </Container>
     </>
   );
-}
+};
 
 export default MovieDetails;
