9c8cc8558432cefeea5531cbca8f71c2c01d9c81
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-29 10:37:35 +0100

    added conditional rendering of Loading/Error/MovieDetails page

--- src/features/movies/MovieDetails/index.js
+++ src/features/movies/MovieDetails/index.js
@@ -7,15 +7,17 @@ import {
   fetchAdditionalData,
   fetchData,
   selectData,
+  selectStatus,
 } from "../../../utils/API/dataSlice";
 import { backdropURL, posterURL } from "../../../utils/API/apiDataURLs";
 import { useAdditionalDataURL } from "../../../utils/API/useAdditionalDataURL";
 import { SmallGridWrapper } from "../../../common/Tile/styled";
 import { Container } from "../../../common/Container";
 import BackdropSection from "./Backdrop";
-import examplePoster from "../../../assets/example_poster.png";
 import examplePerson from "../../../assets/example_person.png";
 import { DetailTile, SmallTile } from "../../../common/Tile";
+import LoadingPage from "../../../common/LoadingPage";
+import ErrorPage from "../../../common/ErrorPage";
 
 const MovieDetails = () => {
   const dispatch = useDispatch();
@@ -28,125 +30,139 @@ const MovieDetails = () => {
   }, [dispatch, dataURL, additionalDataURL]);
 
   const movieDetails = useSelector(selectData);
+  const status = useSelector(selectStatus);
   console.log(movieDetails);
 
-  // Formatting functions
+  // Data formatting functions:
+  const formatYear = (d) => d.split("-")[0];
+
   const formatDate = (dateString) => {
     const [year, month, day] = dateString.split("-");
     return `${day}.${month}.${year}`;
   };
 
-  return (
-    <>
-      {movieDetails.backdrop_path && (
-        <BackdropSection
-          backgroundURL={`${backdropURL}${movieDetails.backdrop_path}`}
-          title={movieDetails.title}
-          votes={movieDetails.vote_count}
-          rate={movieDetails.vote_average.toFixed(1)}
-        />
-      )}
+  const formatCountries = (c) => {
+    return c.map((country) => country.name).join(", ");
+  };
 
-      <Container>
-        {movieDetails.poster_path && (
-          <DetailTile
-            posterPath={`${posterURL}${movieDetails.poster_path}`}
-            title={movieDetails.title}
-            subtitle={movieDetails.release_date.split("-")[0]}
-            firstData={movieDetails.production_countries
-              .map((country) => country.name)
-              .join(", ")}
-            tags={movieDetails.genres}
-            secondData={formatDate(movieDetails.release_date)}
-            votes={movieDetails.vote_count}
-            rate={movieDetails.vote_average.toFixed(1)}
-            isOnBackdrop={false}
-            isOnMainTile={true}
-            overview={movieDetails.overview}
-          />
-        )}
+  const formatRate = (r) => r.toFixed(1);
+  // Rendering logic:
 
-        <Section>
-          <SectionHeader>Cast</SectionHeader>
-          <SmallGridWrapper>
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
+  switch (status) {
+    case "loading":
+      return <LoadingPage />;
+    case "error":
+      return <ErrorPage />;
+    default:
+      return (
+        <>
+          {movieDetails.backdrop_path && (
+            <BackdropSection
+              backgroundURL={`${backdropURL}${movieDetails.backdrop_path}`}
+              title={movieDetails.title}
+              votes={movieDetails.vote_count}
+              rate={formatRate(movieDetails.vote_average)}
             />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-            <SmallTile posterPath="" title="Liu Yifei" subtitle="Mulan" />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-          </SmallGridWrapper>
-        </Section>
-        <Section>
-          <SectionHeader>Crew</SectionHeader>
-          <SmallGridWrapper>
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-            <SmallTile posterPath="" title="Liu Yifei" subtitle="Mulan" />
-            <SmallTile posterPath="" title="Liu Yifei" subtitle="Mulan" />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-            <SmallTile
-              posterPath={examplePerson}
-              title="Liu Yifei"
-              subtitle="Mulan"
-            />
-          </SmallGridWrapper>
-        </Section>
-      </Container>
-    </>
-  );
+          )}
+
+          <Container>
+            {movieDetails.poster_path && (
+              <DetailTile
+                posterPath={`${posterURL}${movieDetails.poster_path}`}
+                title={movieDetails.title}
+                subtitle={formatYear(movieDetails.release_date)}
+                firstData={formatCountries(movieDetails.production_countries)}
+                tags={movieDetails.genres}
+                secondData={formatDate(movieDetails.release_date)}
+                votes={movieDetails.vote_count}
+                rate={formatRate(movieDetails.vote_average)}
+                isOnBackdrop={false}
+                isOnMainTile={true}
+                overview={movieDetails.overview}
+              />
+            )}
+
+            <Section>
+              <SectionHeader>Cast</SectionHeader>
+              <SmallGridWrapper>
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile posterPath="" title="Liu Yifei" subtitle="Mulan" />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+              </SmallGridWrapper>
+            </Section>
+            <Section>
+              <SectionHeader>Crew</SectionHeader>
+              <SmallGridWrapper>
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile posterPath="" title="Liu Yifei" subtitle="Mulan" />
+                <SmallTile posterPath="" title="Liu Yifei" subtitle="Mulan" />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+                <SmallTile
+                  posterPath={examplePerson}
+                  title="Liu Yifei"
+                  subtitle="Mulan"
+                />
+              </SmallGridWrapper>
+            </Section>
+          </Container>
+        </>
+      );
+  }
 };
-
 export default MovieDetails;
