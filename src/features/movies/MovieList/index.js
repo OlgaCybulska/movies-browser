e7d352616f6978dd18459d9088f0f7356135444d
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-29 11:06:45 +0100

    used formatting functions from utils folder

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -9,8 +9,13 @@ import { SectionHeader } from "../../../common/SectionHeader";
 import { GridWrapper } from "../../../common/Tile/styled";
 import { Tile } from "../../../common/Tile";
 import { Pagination } from "../../../common/Pagination";
-import { fetchData, selectData, selectStatus } from "../../../utils/API/dataSlice";
+import {
+  fetchData,
+  selectData,
+  selectStatus,
+} from "../../../utils/API/dataSlice";
 import { StyledLink } from "./styled";
+import { formatYear, formatRate } from "../../../utils/dataFormatFunctions";
 
 export const MovieList = () => {
   const dispatch = useDispatch();
@@ -38,19 +43,22 @@ export const MovieList = () => {
             <Section>
               <SectionHeader>Popular movies</SectionHeader>
               <GridWrapper>
-                {popularMovies.results ? popularMovies.results[0].title && popularMovies.results.map((movie) => (
-                  <li key={movie.id}>
-                    <StyledLink to={`/movies/${movie.id}`}>
-                      <Tile
-                        posterPath={movie.poster_path}
-                        title={movie.original_title}
-                        year={movie.release_date.slice(0, 4)}
-                        rate={movie.vote_average.toFixed(1)}
-                        votes={movie.vote_count}
-                      />
-                    </StyledLink>
-                  </li>
-                )) : null}
+                {popularMovies.results
+                  ? popularMovies.results[0].title &&
+                    popularMovies.results.map((movie) => (
+                      <li key={movie.id}>
+                        <StyledLink to={`/movies/${movie.id}`}>
+                          <Tile
+                            posterPath={movie.poster_path}
+                            title={movie.original_title}
+                            subtitle={formatYear(movie.release_date)}
+                            rate={formatRate(movie.vote_average)}
+                            votes={movie.vote_count}
+                          />
+                        </StyledLink>
+                      </li>
+                    ))
+                  : null}
               </GridWrapper>
             </Section>
           </Container>
