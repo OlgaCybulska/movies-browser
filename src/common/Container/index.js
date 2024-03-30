6212e52ca56602719e29d234c30e4a3f45be33b5
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-30 13:38:32 +0100

    removed unnecessary styling lines

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -3,11 +3,4 @@ import styled from "styled-components";
 export const Container = styled.main`
   max-width: 1368px;
   margin: 0 auto;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.laptopMax}px) {
-    /* margin: 0 16px; */
-  }
-  @media (max-width: ${({ theme }) => theme.breakpoint.largeMobileMax}px) {
-    /* margin: 16px 16px 0 16px; */
-  }
 `;
