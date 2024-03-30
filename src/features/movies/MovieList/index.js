1eaab33087288561b7522b2574a520d12d0b2e85
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-03-30 13:37:25 +0100

    moved motion animation from Tile component to overall MovieList component, added also link to movies as a props

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -6,15 +6,15 @@ import { Section } from "../../../common/Section";
 import ErrorPage from "../../../common/ErrorPage";
 import LoadingPage from "../../../common/LoadingPage";
 import { SectionHeader } from "../../../common/SectionHeader";
-import { GridWrapper } from "../../../common/Tile/styled";
+import { GridWrapper } from "../../../common/GridWrapper/styled";
 import { Tile } from "../../../common/Tile";
 import { Pagination } from "../../../common/Pagination";
+import { motion } from "framer-motion";
 import {
   fetchData,
   selectData,
   selectStatus,
 } from "../../../utils/API/dataSlice";
-import { StyledLink } from "./styled";
 import { formatYear, formatRate } from "../../../utils/dataFormatFunctions";
 
 export const MovieList = () => {
@@ -42,24 +42,29 @@ export const MovieList = () => {
           <Container>
             <Section>
               <SectionHeader>Popular movies</SectionHeader>
-              <GridWrapper>
-                {popularMovies.results
-                  ? popularMovies.results[0].title &&
-                    popularMovies.results.map((movie) => (
-                      <li key={movie.id}>
-                        <StyledLink to={`/movies/${movie.id}`}>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.75 }}
+              >
+                <GridWrapper>
+                  {popularMovies.results
+                    ? popularMovies.results[0].title &&
+                      popularMovies.results.map((movie) => (
+                        <li key={movie.id}>
                           <Tile
+                            link={`/movies/${movie.id}`}
                             posterPath={movie.poster_path}
                             title={movie.original_title}
                             subtitle={formatYear(movie.release_date)}
                             rate={formatRate(movie.vote_average)}
                             votes={movie.vote_count}
                           />
-                        </StyledLink>
-                      </li>
-                    ))
-                  : null}
-              </GridWrapper>
+                        </li>
+                      ))
+                    : null}
+                </GridWrapper>
+              </motion.div>
             </Section>
           </Container>
           <Pagination />
