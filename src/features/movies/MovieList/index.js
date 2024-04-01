b34951c389d8d525b34adc290436679cec90aabc
Kacper Wojda <wojda.kacper@gmail.com>
2024-04-01 21:04:15 +0200

    diplay genre tags

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -14,21 +14,30 @@ import {
   selectData,
   selectStatus,
   selectGenres,
+  fetchGenres
 } from "../../../utils/API/dataSlice";
 import { StyledLink } from "./styled";
+import { genresURL } from "../../../utils/API/apiDataURLs";
 
 export const MovieList = () => {
   const dispatch = useDispatch();
 
   const status = useSelector(selectStatus);
   const dataURL = useDataURL();
+  const genres = useSelector(selectGenres);
 
   useEffect(() => {
     dispatch(fetchData(dataURL));
   }, [dataURL, dispatch]);
 
+  useEffect(() => {
+    if (!genres) {
+      dispatch(fetchGenres(genresURL))
+    }
+  })
+
   const popularMovies = useSelector(selectData);
-  const genres = useSelector(selectGenres);
+
 
   switch (status) {
     case "initial":
@@ -46,24 +55,22 @@ export const MovieList = () => {
               <GridWrapper>
                 {popularMovies.results
                   ? popularMovies.results[0].title &&
-                    popularMovies.results.map((movie) => (
-                      <li key={movie.id}>
-                        <StyledLink to={`/movies/${movie.id}`}>
-                          <Tile
-                            posterPath={movie.poster_path}
-                            title={movie.original_title}
-                            genres={genres.genre_ids && genres.genre_ids.map(
-                              (id) =>
-                                genres.genres.find((genre) => genre.id === id)
-                                  .name
-                            )}
-                            year={movie.release_date.slice(0, 4)}
-                            rate={movie.vote_average.toFixed(1)}
-                            votes={movie.vote_count}
-                          />
-                        </StyledLink>
-                      </li>
-                    ))
+                  popularMovies.results.map((movie) => (
+                    <li key={movie.id}>
+                      <StyledLink to={`/movies/${movie.id}`}>
+                        <Tile
+                          posterPath={movie.poster_path}
+                          title={movie.original_title}
+                          genres={genres && movie.genre_ids.map((id) =>
+                              genres.genres.find((genre) => genre.id === id).name
+                          )}
+                          year={movie.release_date.slice(0, 4)}
+                          rate={movie.vote_average.toFixed(1)}
+                          votes={movie.vote_count}
+                        />
+                      </StyledLink>
+                    </li>
+                  ))
                   : null}
               </GridWrapper>
             </Section>
