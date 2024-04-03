3596b20d3d29c7976dbf14aaa8688fcef34de5bc
Kacper Wojda <wojda.kacper@gmail.com>
2024-04-03 17:20:48 +0200

    working query fetch

--- src/features/SiteHeader/SearchBar/index.js
+++ src/features/SiteHeader/SearchBar/index.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import { InputWrapper, StyledForm, StyledInput, StyledSearchIcon } from "./styled";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSearchContent } from "../NavBar/navBarSlice";
 import { useQueryParameters, useReplaceQueryParameter } from "../../../utils/queryParams";
 import { searchBarParamName } from "../../../utils/searchBarParamName";
+import { fetchData } from "../../../utils/API/dataSlice";
+import { useDataURL } from "../../../utils/API/useDataURL";
 
 const SearchBar = () => {
   const [query, setQuery] = useState(useQueryParameters(searchBarParamName) || "");
   const searchContent = useSelector(selectSearchContent);
   const replaceQueryParameter = useReplaceQueryParameter();
+  const dispatch = useDispatch();
+  const dataURL = useDataURL(query);
 
   const onInputChange = (target) => {
     setQuery(target.value);
@@ -17,6 +21,8 @@ const SearchBar = () => {
       key: searchBarParamName,
       value: target.value.trim() !== "" ? target.value.trim() : "",
     })
+    console.log(dataURL);
+    dispatch(fetchData(dataURL));
   };
 
   const handleSubmit = (event) => {
