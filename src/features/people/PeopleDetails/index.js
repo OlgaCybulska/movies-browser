4cbe936a6ce64afbedd46b87f01e24f9409b9100
Olga Cybulska <olgacybulska7@wp.pl>
2024-04-08 00:25:09 +0200

    switch statement added

--- src/features/people/PeopleDetails/index.js
+++ src/features/people/PeopleDetails/index.js
@@ -16,6 +16,8 @@ import { DetailTile } from "../../../common/Tile";
 import { GridWrapper } from "../../../common/GridWrapper/styled";
 import { Tile } from "../../../common/Tile";
 import { formatRate } from "../../../utils/dataFormatFunctions";
+import LoadingPage from ".././../../common/LoadingPage";
+import ErrorPage from ".././../../common/ErrorPage";
 
 const PeopleDetails = () => {
   const dispatch = useDispatch();
@@ -34,76 +36,91 @@ const PeopleDetails = () => {
   const casts = additionalPeopleDetails.cast;
   const crews = additionalPeopleDetails.crew;
 
-  return (
-    <>
-      <Section>
-        <Container>
-          <DetailTile
-            posterPath={peopleDetails.profile_path}
-            title={peopleDetails.name}
-            firstData={peopleDetails.birthday || "No date of birth available"}
-            secondData={
-              peopleDetails.place_of_birth || "No place of birth available"
-            }
-            overview={peopleDetails.biography || "No biography available"}
-          />
-        </Container>
-      </Section>
+  switch (status) {
+    case "initial":
+      return null;
+    case "loading":
+      return <LoadingPage />;
+    case "error":
+      return <ErrorPage />;
+    case "success":
+      return (
+        <>
+          <Section>
+            <Container>
+              <DetailTile
+                posterPath={peopleDetails.profile_path}
+                title={peopleDetails.name}
+                firstData={
+                  peopleDetails.birthday || "No date of birth available"
+                }
+                secondData={
+                  peopleDetails.place_of_birth || "No place of birth available"
+                }
+                overview={peopleDetails.biography || "No biography available"}
+              />
+            </Container>
+          </Section>
 
-      <Section>
-        <Container>
-          <SectionHeader>
-            {casts ? `Movies - cast (${casts.length})` : "Movies - cast (0)"}
-          </SectionHeader>
-          <GridWrapper>
-            {casts
-              ? casts.length > 0 &&
-                casts.map((cast) => (
-                  <li key={cast.id}>
-                    <Tile
-                      movieTile={false}
-                      posterPath={cast.poster_path}
-                      title={cast.title}
-                      subtitle={cast.release_date}
-                      role={cast.character}
-                      rate={formatRate(cast.vote_average) || ""}
-                      votes={cast.vote_count}
-                      link={`/movies/${cast.id}`}
-                    />
-                  </li>
-                ))
-              : null}
-          </GridWrapper>
-        </Container>
-      </Section>
-      <Section>
-        <Container>
-          <SectionHeader>
-            {crews ? `Movies - crew (${crews.length})` : "Movies - crew (0)"}{" "}
-          </SectionHeader>
-          <GridWrapper>
-            {crews
-              ? crews.length > 0 &&
-                crews.map((crew) => (
-                  <li key={crew.id}>
-                    <Tile
-                      movieTile={false}
-                      posterPath={crew.poster_path}
-                      title={crew.title}
-                      subtitle={crew.year || "No release year available"}
-                      role={crew.job}
-                      rate={crew.vote_average}
-                      votes={crew.vote_count}
-                      link={`/movies/${crew.id}`}
-                    />
-                  </li>
-                ))
-              : null}
-          </GridWrapper>
-        </Container>
-      </Section>
-    </>
-  );
+          <Section>
+            <Container>
+              <SectionHeader>
+                {casts
+                  ? `Movies - cast (${casts.length})`
+                  : "Movies - cast (0)"}
+              </SectionHeader>
+              <GridWrapper>
+                {casts
+                  ? casts.length > 0 &&
+                    casts.map((cast) => (
+                      <li key={cast.id}>
+                        <Tile
+                          movieTile={false}
+                          posterPath={cast.poster_path}
+                          title={cast.title}
+                          subtitle={cast.release_date}
+                          role={cast.character}
+                          rate={formatRate(cast.vote_average) || ""}
+                          votes={cast.vote_count}
+                          link={`/movies/${cast.id}`}
+                        />
+                      </li>
+                    ))
+                  : null}
+              </GridWrapper>
+            </Container>
+          </Section>
+          <Section>
+            <Container>
+              <SectionHeader>
+                {crews
+                  ? `Movies - crew (${crews.length})`
+                  : "Movies - crew (0)"}{" "}
+              </SectionHeader>
+              <GridWrapper>
+                {crews
+                  ? crews.length > 0 &&
+                    crews.map((crew) => (
+                      <li key={crew.id}>
+                        <Tile
+                          movieTile={false}
+                          posterPath={crew.poster_path}
+                          title={crew.title}
+                          subtitle={crew.year || "No release year available"}
+                          role={crew.job}
+                          rate={crew.vote_average}
+                          votes={crew.vote_count}
+                          link={`/movies/${crew.id}`}
+                        />
+                      </li>
+                    ))
+                  : null}
+              </GridWrapper>
+            </Container>
+          </Section>
+        </>
+      );
+  }
 };
 
 export default PeopleDetails;
