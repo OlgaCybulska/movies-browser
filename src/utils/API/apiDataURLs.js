66f7c638b0ffdc976745854213e648ac57ec2b10
OlgaCybulska <143279143+OlgaCybulska@users.noreply.github.com>
2024-04-08 15:36:33 +0200

    Update apiDataURLs.js
    double link removed

--- src/utils/API/apiDataURLs.js
+++ src/utils/API/apiDataURLs.js
@@ -4,7 +4,7 @@ export const backdropURL = `${URLImages}w1920_and_h800_multi_faces`;
 
 export const genresURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=741487ab9378f09398cb15784041196d&language=en-US"
 
-export const backdropURL = `${URLImages}w1920_and_h800_multi_faces`;
+
 
 
 // this file is a place for API urls, for example: backdrop image which will be used in MovieDetails has different URL than poster image.
