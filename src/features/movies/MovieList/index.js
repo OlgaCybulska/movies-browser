e5f67fc1d98e1a3968f723509bf56a9b06d4fd7a
Kacper Wojda <wojda.kacper@gmail.com>
2024-04-09 21:03:08 +0200

    no results page render logic

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -21,6 +21,7 @@ import { genresURL } from "../../../utils/API/apiDataURLs";
 import { formatYear, formatRate } from "../../../utils/dataFormatFunctions";
 import { useQueryParameters } from "../../../utils/queryParams";
 import { searchBarParamName } from "../../../utils/searchBarParamName";
+import NoResultsPage from "../../../common/NoResultsPage";
 
 export const MovieList = () => {
   const dispatch = useDispatch();
@@ -64,28 +65,28 @@ export const MovieList = () => {
                 <GridWrapper>
                   {popularMovies.results.length !== 0
                     ? popularMovies.results[0].title &&
-                      popularMovies.results.map((movie) => (
-                        <li key={movie.id}>
-                          <Tile
-                            link={`/movies/${movie.id}`}
-                            posterPath={movie.poster_path}
-                            title={movie.original_title}
-                            genres={
-                              genres.genres &&
-                              movie.genre_ids.map(
-                                (id) =>
-                                  genres.genres.find((genre) => genre.id === id)
-                                    .name
-                              )
-                            }
-                            subtitle={formatYear(movie.release_date)}
-                            movieTile={true}
-                            rate={formatRate(movie.vote_average)}
-                            votes={movie.vote_count}
-                          />
-                        </li>
-                      ))
-                    : null}
+                    popularMovies.results.map((movie) => (
+                      <li key={movie.id}>
+                        <Tile
+                          link={`/movies/${movie.id}`}
+                          posterPath={movie.poster_path}
+                          title={movie.original_title}
+                          genres={
+                            genres.genres &&
+                            movie.genre_ids.map(
+                              (id) =>
+                                genres.genres.find((genre) => genre.id === id)
+                                  .name
+                            )
+                          }
+                          subtitle={formatYear(movie.release_date)}
+                          movieTile={true}
+                          rate={formatRate(movie.vote_average)}
+                          votes={movie.vote_count}
+                        />
+                      </li>
+                    ))
+                    : <NoResultsPage />}
                 </GridWrapper>
               </motion.div>
             </Section>
